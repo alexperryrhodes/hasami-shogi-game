a7114ae7abadf677af207503a192c1aea9f9b7ab
Alexandra Rhodes <atperry30@gmail.com>
2021-11-23 14:31:05 -0800

    Added logic

--- HasamiShogiGame.py
+++ HasamiShogiGame.py
@@ -14,12 +14,13 @@ class HasamiShogiGame:
 
         self._board = Board(self._pieces)
 
-        self._red = Player(self._red_pieces)
-        self._black = Player(self._black_pieces)
+        self._red_player = Player(self._red_pieces, 'RED')
+        self._black_player = Player(self._black_pieces, 'BLACK')
 
         self._game_state = 'UNFINISHED'
 
-        self._active_player = 'BLACK'
+        self._active_player = self._black_player
+        self._inactive_player = self._red_player
 
     def get_game_state(self):
         """"""
@@ -27,54 +28,29 @@ class HasamiShogiGame:
 
     def get_active_player(self):
         """"""
-        return self._active_player
+        return self._active_player.get_player_color()
 
     def get_num_captured_pieces(self, color):
         """"""
         if color == 'RED':
-            return self._red.get_captured_pieces
+            return self._red_player.get_captured_pieces
         elif color == 'BLACK':
-            return self._black.get_captured_pieces
+            return self._black_player.get_captured_pieces
         else:
             print('Not a valid color, please try again.')
 
-    def make_move(self, square_from, square_to):
+    def get_square_occupant(self, text_square):
         """"""
-        # Confirm active player is making move
-        square_from_occupant = self._board.get_piece(square_from)
-        square_from_occupant_color = square_from_occupant.get_color()
-        if self._active_player != square_from_occupant_color:
-            print('Error: Not active player')
-            return False
-
-        # Confirm move is Valid: orthogonal and along a clear path
-        board_bool = self._board.validate_move(square_from, square_to)
-        if board_bool is False:
-            print('Error: move was not valid')
-
-        # If move is valid, update square occupants
-        current_square = self._board.get_square(square_from)
-        target_square = self._board.get_square(square_to)
-        current_square.set_occupant(None)
-        target_square.set_occupant(square_from_occupant)
-
-        # If move is valid: switch turns
-        if self._active_player == 'BLACK':
-            self._active_player = 'RED'
-        else:
-            self._active_player = 'BLACK'
-
-    def get_square_occupant(self, square):
-        """"""
-        piece = self._board.get_piece()
+        square = self._board.board_map(text_square)
+        piece = self._board.get_piece(square)
         return piece.get_color()
 
     def generate_pieces(self):
         """"""
         piece_list = []
-        for row in zip(['a', 'i'], ['RED', 'BLACK']):
-            for column in range(1, 10):
-                piece_list.append(Piece(row[0], str(column), row[1]))
+        for row in zip([0, 8], ['RED', 'BLACK']):
+            for column in range(0, 9):
+                piece_list.append(Piece(row[0], column, row[1]))
 
         return piece_list
 
@@ -95,9 +71,53 @@ class HasamiShogiGame:
 
         return black_list
 
-    def print_board(self):
-        self._board.print_board()
+    def make_move(self, text_square_from, text_square_to):
+        """"""
+        # Confirm game is still unfinished
+        if self._game_state != 'UNFINISHED':
+            print("Error: Game has already been won")
+            return False
 
+        current = self._board.board_map(text_square_from)
+        target = self._board.board_map(text_square_to)
+
+        # Confirm active player is making move
+        piece = self._board.get_piece(current)
+        piece_color = piece.get_color()
+        player_color = self._active_player.get_player_color()
+        if player_color != piece_color:
+            print('Error: Not active player')
+            return False
+
+        # Confirm move is Valid: orthogonal and along a clear path
+        board_bool = self._board.validate_move(current, target)
+        if board_bool is False:
+            print('Error: move was not valid')
+
+        # If move is valid, update square occupants
+        current_square = self._board.get_square(current)
+        target_square = self._board.get_square(target)
+        current_square.set_occupant(None)
+        target_square.set_occupant(piece)
+
+        # If move is valid, update piece coordinates
+        piece.set_row(target[0])
+        piece.set_column(target[1])
+
+        # If move is valid, check for capture
+        active_pieces = self._active_player.get_active_pieces()
+        inactive_pieces = self._inactive_player.get_active_pieces()
+        captured_squares = self._board.check_capture(active_pieces, inactive_pieces, target)
+
+        # If move is valid: switch turns
+        active_player = self._active_player
+        inactive_player = self._inactive_player
+
+        self._active_player = inactive_player
+        self._inactive_player = active_player
+
+    def print_board(self):
+        return self._board.print_board()
 
 class Board:
     """Represents"""
@@ -116,10 +136,10 @@ class Board:
         column_map = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8}
 
         # Create an empty board
-        for _ in row_map:
+        for row in row_map:
             board_row = []
-            for _ in column_map:
-                board_row.append(Square(None))
+            for column in column_map:
+                board_row.append(Square(row, column, None))
             board.append(list(board_row))
 
         # Designate corner spaces
@@ -130,24 +150,22 @@ class Board:
         # Place pieces on board
         for piece in pieces:
             row = piece.get_row()
-            row_index = row_map.get(row)
             column = piece.get_column()
-            column_index = column_map.get(column)
-            board[row_index][column_index].set_occupant(piece)
+            board[row][column].set_occupant(piece)
 
         return board
 
-    def board_map(self, square):
+    def board_map(self, text_square):
         row_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8}
         column_map = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8}
 
-        square_row = square[:1]
-        square_column = square[1:]
+        square_row = text_square[:1]
+        square_column = text_square[1:]
 
         row = row_map.get(square_row)
         column = column_map.get(square_column)
 
-        return (row, column)
+        return self._board[row][column]
 
     def print_board(self):
         """Prints the Board object to the console"""
@@ -168,12 +186,12 @@ class Board:
                 else:
                     print_row += '[ ]'
             print(print_row)
+        print()
 
     def get_square(self, square):
         """"""
-        location = self.board_map(square)
-        row = location[0]
-        column = location[1]
+        row = square[0]
+        column = square[1]
 
         square = self._board[row][column]
 
@@ -181,56 +199,49 @@ class Board:
 
     def get_piece(self, square):
         """"""
-        location = self.board_map(square)
-        row = location[0]
-        column = location[1]
+        row = square[0]
+        column = square[1]
 
         square = self._board[row][column]
 
         piece = square.get_occupant()
 
-        if piece is None:
-            return 'NONE'
-        else:
-            return piece
+        return piece
 
-    def validate_move(self, square_from, square_to):
+    def validate_move(self, current, target):
         """"""
-        current_square = self.board_map(square_from)
-        target_square = self.board_map(square_to)
-
         # Checks if move is orthogonal
-        move_direction = self.move_direction(current_square, target_square)
+        move_direction = self.check_direction(current, target)
         if move_direction == 'Invalid':
             return False
 
         # Checks if path between squares is clear
-        path = self.empty_path_check(move_direction,current_square, target_square)
+        path = self.check_path(move_direction, current, target)
         if path == 'Occupied':
             return False
 
         return True
 
-    def move_direction(self, current_square, target_square):
+    def check_direction(self, current, target):
 
-        if (current_square[0] != target_square[0]) and (current_square[1] != target_square[1]):
-            move_direction = 'Invalid'
+        if (current[0] != target[0]) and (current[1] != target[1]):
+            direction = 'Invalid'
 
-        elif current_square[0] == target_square[0]:
-            if current_square[1] - target_square[1] < 0:
-                move_direction = ('Horizontal', 'Left')
+        elif current[0] == target[0]:
+            if current[1] - target[1] < 0:
+                direction = ('Horizontal', 'Left')
             else:
-                move_direction = ('Horizontal', 'Right')
+                direction = ('Horizontal', 'Right')
 
-        elif current_square[1] == target_square[1]:
-            if current_square[0] - target_square[0] < 0:
-                move_direction = ('Vertical', 'Down')
+        elif current[1] == target[1]:
+            if current[0] - target[0] < 0:
+                direction = ('Vertical', 'Down')
             else:
-                move_direction = ('Vertical', 'Up')
+                direction = ('Vertical', 'Up')
 
-        return move_direction
+        return direction
 
-    def empty_path_check(self, direction,  current, target):
+    def check_path(self, direction, current, target):
         path = 'Clear'
         c_row = current[0]
         c_column = current[1]
@@ -261,12 +272,93 @@ class Board:
 
         return path
 
+    def check_capture(self, friendly_pieces, enemy_pieces, target):
+        """"""
+        t_row = target[0]
+        t_column = target[1]
+
+        row_friendly = []
+        column_friendly = []
+        row_enemy = []
+        column_enemy = []
+
+        # Generate list of coordinates of all friendly pieces in same row and column
+        for piece in friendly_pieces:
+            row = piece.get_row()
+            column = piece.get_column()
+
+            # Skip over adding the target piece to the list
+            if t_row == row and t_column == column:  # don't add target piece
+                continue
+
+            # If friendly piece is in same row as target, add piece's column to list
+            if t_row == row:
+                row_friendly.append(column)
+
+            # If friendly piece is in same column as target, add piece's row to list
+            elif t_column == column:
+                column_friendly.append(row)
+
+        # Generate list of coordinates of all enemy pieces in same row and column
+        for piece in enemy_pieces:
+            # If enemy piece is in same row as target, add piece's column to list
+            if t_row == piece.get_row():
+                row_enemy.append(piece.get_column())
+
+            # If enemy piece is in same column as target, add piece's row to list
+            elif t_column == piece.get_row():
+                column_enemy.append(piece.get_column())
+
+        print('RF', row_friendly)
+        print('CF', column_friendly)
+        print('RE', row_enemy)
+        print('CE', column_enemy)
+        print('RT', t_row)
+        print('CT', t_column)
+
+        capture_list = []
+
+        looking_state = 0
+        capture_state = 0
+
+        x = 1
+        if t_row + x in row_enemy:
+            looking_state = 1
+            x +=1
+        while looking_state == 1 and capture_state == 0:
+            if t_row + x in row_friendly:
+                capture_state = 1
+            elif t_row + x in row_enemy:
+                looking_state = 1
+            else:
+                looking_state = 0
+
+    def board_state(self):
+        square_list = list(range(9))
+        red_pieces = []
+        black_pieces = []
+
+        for row in square_list:
+            for column in square_list:
+                location = (row, column)
+                piece = self.get_piece(location)
+                if piece is None:
+                    continue
+                elif piece.get_color() == 'RED':
+                    red_pieces.append(location)
+                else:
+                    black_pieces.append(location)
+
+        return red_pieces, black_pieces
+
 
 class Square:
     """Represents a space located on the Board"""
 
-    def __init__(self, occupant, corner=False):
+    def __init__(self, row, column, occupant, corner=False):
         """Creates a initial Square object"""
+        self._row = row
+        self._column = column
         self._occupant = occupant
         self._corner = corner
 
@@ -319,10 +411,14 @@ class Piece:
 class Player:
     """Represents"""
 
-    def __init__(self, pieces):
+    def __init__(self, pieces, color):
         """Creates a Player object"""
         self._active_pieces = pieces
         self._captured_pieces = []
+        self._color = color
+
+    def get_player_color(self):
+        return self._color
 
     def get_active_pieces(self):
         """"""
@@ -337,39 +433,55 @@ class Player:
         pass
 
 
-game = HasamiShogiGame()
-game.print_board()
-print('New Game\n')
+def main():
+    game = HasamiShogiGame()
 
-game.make_move('i1', 'f1')
-game.print_board()
-print('Black moves Vertical Up\n')
+    game.make_move('i4', 'c4')
+    #game.print_board()
 
-game.make_move('a1', 'e1')
-game.print_board()
-print('Red moves Vertical Down\n')
+    game.make_move('a2', 'e2')
+    #game.print_board()
 
-game.make_move('f1', 'f6')
-game.print_board()
-print('Black moves Horizontal Right\n')
+    game.make_move('c4', 'c5')
+    #game.print_board()
 
-game.make_move('e1', 'e9')
-game.print_board()
-print('Red moves Horizontal Right\n')
+    game.make_move('a4', 'e4')
+    #game.print_board()
 
-game.make_move('f6', 'g6')
-game.print_board()
-print('Black moves Vertical Down\n')
+    game.make_move('i3', 'e3')
+    #game.print_board()
 
-game.make_move('e9', 'c9')
-game.print_board()
-print('Red moves Vertical Up\n')
+    game.make_move('a6', 'b6')
+    #game.print_board()
 
-game.make_move('g6', 'g2')
-game.print_board()
-print('Black moves Horizontal Left\n')
+    game.make_move('i5', 'd5')
+    #game.print_board()
 
-game.make_move('c9', 'c5')
-game.print_board()
-print('Red moves Horizontal Left\n')
+    game.make_move('b6', 'b5')
+    #game.print_board()
 
+    game.make_move('i6', 'e6')
+    #game.print_board()
+
+    game.make_move('a7', 'e7')
+    #game.print_board()
+
+    game.make_move('i9', 'h9')
+    #game.print_board()
+
+    game.make_move('a8', 'f8')
+    #game.print_board()
+
+    game.make_move('h9', 'i9')
+    #game.print_board()
+
+    game.make_move('f8', 'f5')
+    #game.print_board()
+
+    game.make_move('i9', 'h9')
+    #game.print_board()
+
+    game.make_move('f5', 'e5')
+    game.print_board()
+
+main()
